c2d750d149277a53709d4cabbc91dac56b04107f
kid-us <kidushh29@gmail.com>
2024-06-07 12:41:44 +0300

    Dev env't setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <div className="text-white mt-16">
+          <h1 className="text-3xl">VibeCard</h1>
+          <p>Welcome to VibeCard, VibeCard development started</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
