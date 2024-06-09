cb4b3b9c47db8dec7b027eaf08853015c14bfed7
kid-us <kidushh29@gmail.com>
2024-06-09 23:56:48 +0300

    Routing

--- src/components/Pages/Login.tsx
+++ src/components/Pages/Login.tsx
@@ -1,8 +1,14 @@
 const Login = () => {
   return (
-    <div className="text-white">
-      <p>Login</p>
+    // <div className="container mx-auto">
+    <div className="flex justify-center h-[100vh]">
+      <div className="content-center">
+        <div className="">
+          <p className="">Login</p>
+        </div>
+      </div>
     </div>
+    // </div>
   );
 };
 
