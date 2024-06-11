ce3586d6746f79ef6c7877c46730ac6f0a8d4b33
kid-us <kidushh29@gmail.com>
2024-06-11 19:09:50 +0300

    Hero Component and HEro Card

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -12,17 +12,6 @@ const Home = () => {
         <div className="lg:mt-36 mt-28">
           <Hero />
         </div>
-        <div className="mt-16">
-          <p>Home</p> <br />
-          <h1 className="text-3xl">VibeCard</h1>
-          <p>Welcome to VibeCard, development started</p>
-          <br />
-          <div className="text-blue-500">
-            <Link to="/login">Login</Link> <br />
-            <Link to="/register">Register</Link>
-          </div>
-        </div>
-        <p>lorem1000x</p>
       </div>
     </>
   );
