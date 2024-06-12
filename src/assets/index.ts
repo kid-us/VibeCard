752cabc49a31f23859d990f66342243934cc6ebc
kid-us <kidushh29@gmail.com>
2024-06-12 12:31:35 +0300

    Page not found

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,5 +3,6 @@ import qrCode from "./qr-code.png";
 import user from "./user.png";
 import google from "./google.png";
 import hero from "./hero.png";
+import pageNotFound from "./page404.gif";
 
-export { cardBg, qrCode, user, google, hero };
+export { cardBg, qrCode, user, google, hero, pageNotFound };
--- src/components/Pages/Page404.tsx
+++ src/components/Pages/Page404.tsx
@@ -1,11 +1,42 @@
+import { Link } from "react-router-dom";
+import { pageNotFound } from "../../assets";
+
 const Page404 = () => {
   return (
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi fugit
-      sequi architecto animi consequuntur non exercitationem tempora soluta
-      quisquam aliquam praesentium ipsam harum enim, maiores temporibus? Sit
-      voluptate sint ad?
-    </p>
+    <div className="px-40 menu-bg">
+      <div className="flex justify-center h-[100dvh]">
+        <div className="content-center">
+          <div className="w-[500px] h-64 bg-zinc-800 rounded relative p-3 shadow-lg shadow-zinc-950">
+            <Link to="/" className="logo-font text-teal-400 text-2xl">
+              vibecard
+            </Link>
+            <div className="grid grid-cols-2 gap-5">
+              <div className="mt-3">
+                <img
+                  src={pageNotFound}
+                  alt="not found"
+                  className="grayscale rounded-md"
+                />
+              </div>
+              <div className="mt-3 font-extrabold">
+                <p className="text-2xl font-poppins text-white">404</p>
+                <p className="font-poppins mb-5 text-gray-300">
+                  Page Not Found
+                </p>
+                <p className="text-xs font-extralight text-gray-200">
+                  The page you are looking for can't be found.
+                </p>
+                <Link to="/">
+                  <div className="mt-5 w-full bg-sky-700 py-2 rounded text-center font-semibold text-sm text-gray-200 shadow shadow-gray-500">
+                    Back to Home
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
