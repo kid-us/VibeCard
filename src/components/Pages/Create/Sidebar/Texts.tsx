3c46ebf7366aa804344f351882fa5f6bfcd73fa0
kid-us <kidushh29@gmail.com>
2024-06-15 15:35:55 +0300

    Dynamic Font styles and Sizes

--- src/components/Pages/Create/Sidebar/Texts.tsx
+++ src/components/Pages/Create/Sidebar/Texts.tsx
@@ -85,46 +85,6 @@ const Texts = () => {
               view={view}
               defaultFontSize={pronoun.size}
             />
-            {/* <div className="flex my-5">
-              <p className="first-letter:uppercase text-teal-400">{view}</p>
-              <p className="ms-2 text-white text-xs"> Font Style</p>
-            </div>
-
-            <div className="bg-white w-full rounded p-2">
-              {fonts.map((font) => (
-                <p
-                  key={font.name}
-                  onClick={() =>
-                    handleFontStyle(font.style, font.name, view as keyof State)
-                  }
-                  className={`${font.style} hover:gray-300 hover:text-teal-400
-            cursor-pointer  pb-2 ${
-              pronoun.font === font.style ? "text-sky-600" : "text-black"
-            }`}
-                >
-                  {font.name}
-                </p>
-              ))}
-            </div> */}
-            {/* Font Sizes */}
-            {/* <div className="flex my-3">
-              <p className="first-letter:uppercase text-teal-600">{view}</p>
-              <p className=" ms-2 text-white text-xs"> Font Size</p>
-            </div>
-            <div className="bg-white w-full rounded p-2">
-              {fontSize.map((size) => (
-                <p
-                  key={size.name}
-                  onClick={() => updateSize(view as keyof State, size.size)}
-                  className={`${size.size} hover:gray-300 hover:text-teal-400
-            cursor-pointer  pb-2 ${
-              pronoun.size === size.size ? "text-teal-400" : "text-black"
-            }`}
-                >
-                  {size.name}
-                </p>
-              ))}
-            </div> */}
           </>
         )}
       </div>
