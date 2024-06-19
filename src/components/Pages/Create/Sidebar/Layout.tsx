a11076e631d2d19d01f35caec337bf49bad68810
kid-us <kidushh29@gmail.com>
2024-06-19 17:56:41 +0300

    Responsive Layout

--- src/components/Pages/Create/Sidebar/Layout.tsx
+++ src/components/Pages/Create/Sidebar/Layout.tsx
@@ -8,65 +8,73 @@ const Layout = () => {
       <p className="chakra text-white mb-4">Card Layouts</p>
       <div className="bg-white rounded p-2">
         <div>
-          <p className="chakra">Default</p>
+          <p className="chakra text-black">Default</p>
           <div
             onClick={() => updateLayout("default")}
             className="relative shadow bg-gray-100 shadow-zinc-800 rounded-lg mt-3 cursor-pointer"
           >
-            <div className="w-full h-20 bg-gray-600 rounded ">
+            <div className="w-full lg:h-20 h-32 bg-gray-600 rounded ">
               <div className={`h-24 relative flex justify-between p-2 z-0`}>
-                <div className="absolute top-10 w-16 h-16 border-[4px] rounded-full border-white overflow-hidden z-0">
+                <div className="absolute lg:top-10 top-16 lg:h-16 lg:w-16 w-24 h-24 border-[4px] rounded-full border-white overflow-hidden z-0">
                   <img src={user} alt="user" />
                 </div>
                 {/* Pronoun and Name */}
-                <div className="content-center mb-2">
+                <div className="content-center lg:mb-2 ">
                   <p
-                    className={`absolute right-0 me-2 bg-gray-400 w-36 h-8 text-center rounded`}
+                    className={`absolute lg:top-10 top-20 right-0 lg:w-36 lg:h-8 w-44 h-10 bg-gray-400 text-center rounded`}
                   ></p>
                 </div>
               </div>
             </div>
-            <div className="px-5 mt-10 text-white">
+            <div className="px-5 mt-10 ">
               <div className="relative">
                 <img
                   src={user}
                   alt="Cover"
-                  className="absolute right-0 -top-7 w-14 h-14 rounded-full border-2 border-white"
+                  className="absolute right-0 -top-7 lg:w-14 lg:h-14 w-16 h-16 rounded-full border-2 border-white"
                 />
                 {/* Job Title */}
-                <p className={`mb-1 bg-gray-400 w-44 h-6 rounded`}></p>
+                <p
+                  className={`mb-1 bg-gray-400 lg:w-44 lg:h-6 w-52 h-10 rounded`}
+                ></p>
                 {/* Company */}
-                <p className={`mb-1 bg-gray-400 w-44 h-6 rounded`}></p>
+                <p
+                  className={`mb-1 bg-gray-400 lg:w-44 lg:h-6 w-6/2 h-10 rounded`}
+                ></p>
                 {/* Tag Line */}
-                <p className={`mt-3 mb-1 bg-gray-400 w-44 h-6 rounded`}></p>
+                <p
+                  className={`mt-3 mb-1 bg-gray-400 lg:w-44 lg:h-6 w-6/2 h-10 rounded`}
+                ></p>
                 {/* Location */}
-                <p className={`my-2 mb-1 bg-gray-400 w-44 h-6 rounded`}></p>
+                <p
+                  className={`my-2 mb-1 bg-gray-400 lg:w-44 lg:h-6 w-6/2 h-10 rounded`}
+                ></p>
               </div>
             </div>
-            <div className="grid grid-cols-5 px-3 mt-4 pb-4">
-              <p className="bi-envelope-fill text-3xl text-gray-400"></p>
-              <p className="bi-telegram text-3xl text-gray-400"></p>
-              <p className="bi-whatsapp text-3xl text-gray-400"></p>
-              <p className="bi-globe text-3xl text-gray-400"></p>
-              <p className="bi-telephone-fill text-3xl text-gray-400"></p>
+            <div className="grid grid-cols-5 lg:ps-3 ps-6 mt-6 pb-5">
+              <p className="bi-envelope-fill lg:text-3xl text-4xl lg:text-gray-400 text-gray-500"></p>
+              <p className="bi-telegram lg:text-3xl text-4xl lg:text-gray-400 text-gray-500"></p>
+              <p className="bi-whatsapp lg:text-3xl text-4xl lg:text-gray-400 text-gray-500"></p>
+              <p className="bi-globe lg:text-3xl text-4xl lg:text-gray-400 text-gray-500"></p>
+              <p className="bi-telephone-fill lg:text-3xl text-4xl lg:text-gray-400 text-gray-500"></p>
             </div>
           </div>
 
           {/* Right */}
-          <p className="mt-5 chakra">Right</p>
+          <p className="mt-5 chakra text-black">Right</p>
           <div
             onClick={() => updateLayout("right")}
             className="relative bg-gray-100 shadow shadow-zinc-800 rounded-lg mt-3 cursor-pointer"
           >
-            <div className="w-full h-20 bg-gray-600 rounded ">
+            <div className="w-full lg:h-20 h-32 bg-gray-600 rounded ">
               <div className={`h-24 relative flex justify-between p-2 z-0`}>
-                <div className="absolute top-10 right-0 w-16 h-16 border-[4px] rounded-full border-white overflow-hidden z-0">
+                <div className="absolute right-2 lg:top-10 top-16 lg:h-16 lg:w-16 w-24 h-24 border-[4px] rounded-full border-white overflow-hidden z-0">
                   <img src={user} alt="user" />
                 </div>
                 {/* Pronoun and Name */}
                 <div className="content-center mb-2">
                   <p
-                    className={`absolute left-2 bg-gray-400 w-36 h-8 text-center rounded`}
+                    className={`absolute left-2 lg:top-10 top-20 lg:w-36 lg:h-8 w-44 h-10 bg-gray-400 text-center rounded`}
                   ></p>
                 </div>
               </div>
@@ -76,42 +84,50 @@ const Layout = () => {
                 <img
                   src={user}
                   alt="Cover"
-                  className="absolute left-0 -top-7 w-14 h-14 rounded-full border-2 border-white"
+                  className="absolute left-0 -top-7 lg:w-14 lg:h-14 w-16 h-16 rounded-full border-2 border-white"
                 />
                 {/* Job Title */}
-                <p className={`mb-1 bg-gray-400 w-40 ms-4 h-6 rounded`}></p>
+                <p
+                  className={`mb-1 bg-gray-400 lg:w-44 lg:h-6 w-52 h-10 lg:ms-0 ms-[4.2em] rounded`}
+                ></p>
                 {/* Company */}
-                <p className={`mb-1 bg-gray-400 w-44 h-6 rounded`}></p>
+                <p
+                  className={`mb-1 bg-gray-400 lg:w-44 lg:h-6 w-6/2 h-10  rounded`}
+                ></p>
                 {/* Tag Line */}
-                <p className={`mt-3 mb-1 bg-gray-400 w-44 h-6 rounded`}></p>
+                <p
+                  className={`mt-3 mb-1 bg-gray-400 lg:w-44 lg:h-6 w-6/2 h-10  rounded`}
+                ></p>
                 {/* Location */}
-                <p className={`my-2 mb-1 bg-gray-400 w-44 h-6 rounded`}></p>
+                <p
+                  className={`my-2 mb-1 bg-gray-400 lg:w-44 lg:h-6 w-6/2 h-10  rounded`}
+                ></p>
               </div>
             </div>
-            <div className="grid grid-cols-5 px-3 mt-4 pb-4">
-              <p className="bi-envelope-fill text-3xl text-gray-400"></p>
-              <p className="bi-telegram text-3xl text-gray-400"></p>
-              <p className="bi-whatsapp text-3xl text-gray-400"></p>
-              <p className="bi-globe text-3xl text-gray-400"></p>
-              <p className="bi-telephone-fill text-3xl text-gray-400"></p>
+            <div className="grid grid-cols-5 lg:ps-3 ps-6 mt-6 pb-5">
+              <p className="bi-envelope-fill lg:text-3xl text-4xl lg:text-gray-400 text-gray-500"></p>
+              <p className="bi-telegram lg:text-3xl text-4xl lg:text-gray-400 text-gray-500"></p>
+              <p className="bi-whatsapp lg:text-3xl text-4xl lg:text-gray-400 text-gray-500"></p>
+              <p className="bi-globe lg:text-3xl text-4xl lg:text-gray-400 text-gray-500"></p>
+              <p className="bi-telephone-fill lg:text-3xl text-4xl lg:text-gray-400 text-gray-500"></p>
             </div>
           </div>
 
           {/* Center */}
-          <p className="mt-5 chakra">Center</p>
+          <p className="mt-5 chakra text-black">Center</p>
           <div
             onClick={() => updateLayout("centered")}
             className="relative bg-gray-100 shadow shadow-zinc-800 rounded-lg mt-3 cursor-pointer mb-5"
           >
-            <div className="w-full h-20 bg-gray-600 rounded ">
+            <div className="w-full lg:h-20 h-32 bg-gray-600 rounded">
               <div className={`h-24 relative flex justify-between p-2 z-0`}>
-                <div className="absolute top-10 right-20 w-16 h-16 border-[4px] rounded-full border-white overflow-hidden z-0">
+                <div className="absolute lg:top-10 lg:right-20 lg:w-16 lg:h-16 top-10 w-28 h-28 left-24 border-[4px] rounded-full border-white overflow-hidden z-0">
                   <img src={user} alt="user" />
                 </div>
                 <img
                   src={user}
                   alt="Cover"
-                  className="absolute right-20 top-20 w-10 h-10 rounded-full border-2 border-white"
+                  className="absolute lg:right-10 lg:top-20 lg:w-10 lg:h-10 w-14 h-14 right-24 top-28 rounded-full border-2 border-white"
                 />
               </div>
             </div>
@@ -119,26 +135,32 @@ const Layout = () => {
               <div className="relative">
                 {/* Pronoun and Name */}
                 <p
-                  className={`bg-gray-400 w-40  ms-3 h-6 text-center rounded mt-12 mb-2`}
+                  className={`bg-gray-400 lg:w-44 lg:h-6 w-6/2 h-10 text-center rounded mt-12 mb-2`}
                 ></p>
                 {/* Job Title */}
-                <p className={`mb-1 bg-gray-400 w-40 ms-3 h-6 rounded`}></p>
+                <p
+                  className={`mb-1 bg-gray-400 lg:w-44 lg:h-6 w-6/2 h-10 rounded`}
+                ></p>
                 {/* Company */}
-                <p className={`mb-1 bg-gray-400 w-40 ms-3 h-6 rounded`}></p>
+                <p
+                  className={`mb-1 bg-gray-400 lg:w-44 lg:h-6 w-6/2 h-10 rounded`}
+                ></p>
                 {/* Tag Line */}
-                <p className={`mb-1 bg-gray-400 w-40 ms-3 h-6 rounded`}></p>
+                <p
+                  className={`mb-1 bg-gray-400 lg:w-44 lg:h-6 w-6/2 h-10 rounded`}
+                ></p>
                 {/* Location */}
                 <p
-                  className={`my-2 mb-1 bg-gray-400 w-40 ms-3 h-6 rounded`}
+                  className={`my-2 mb-1 bg-gray-400 lg:w-44 lg:h-6 w-6/2 h-10 rounded`}
                 ></p>
               </div>
             </div>
-            <div className="grid grid-cols-5 px-3 mt-4 pb-4">
-              <p className="bi-envelope-fill text-3xl text-gray-400"></p>
-              <p className="bi-telegram text-3xl text-gray-400"></p>
-              <p className="bi-whatsapp text-3xl text-gray-400"></p>
-              <p className="bi-globe text-3xl text-gray-400"></p>
-              <p className="bi-telephone-fill text-3xl text-gray-400"></p>
+            <div className="grid grid-cols-5 lg:ps-3 ps-6 mt-6 pb-5">
+              <p className="bi-envelope-fill lg:text-3xl text-4xl lg:text-gray-400 text-gray-500"></p>
+              <p className="bi-telegram lg:text-3xl text-4xl lg:text-gray-400 text-gray-500"></p>
+              <p className="bi-whatsapp lg:text-3xl text-4xl lg:text-gray-400 text-gray-500"></p>
+              <p className="bi-globe lg:text-3xl text-4xl lg:text-gray-400 text-gray-500"></p>
+              <p className="bi-telephone-fill lg:text-3xl text-4xl lg:text-gray-400 text-gray-500"></p>
             </div>
           </div>
         </div>
