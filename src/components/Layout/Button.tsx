e2dd114b94e02cd84025abf769bf82b9a52e081a
kid-us <kidushh29@gmail.com>
2024-06-19 20:16:23 +0300

    Space between Save button

--- src/components/Layout/Button.tsx
+++ src/components/Layout/Button.tsx
@@ -4,7 +4,7 @@ const Button = () => {
   const { button } = useTextColorStore();
   return (
     <button
-      className={`w-full rounded-lg py-4 mb-9 mt-5 ${
+      className={`w-full rounded-lg py-4 mb-14 mt-6 ${
         button.color === "#000000" ? "text-white" : "text-black"
       }  shadow-md font-poppins font-extrabold shadow-zinc-50`}
       style={{ backgroundColor: button.color }}
