b3693c5b692d4c3e8ed917bf6d989793e03a47ae
kid-us <kidushh29@gmail.com>
2024-06-21 15:56:33 +0300

    User authentications

--- src/components/Navbar/Menu.tsx
+++ src/components/Navbar/Menu.tsx
@@ -13,9 +13,8 @@ const Menu = ({ nav, menu }: Props) => {
   return (
     <>
       <div className="flex justify-between py-3 px-3">
-        <div>
+        <div onClick={() => menu(false)}>
           <p
-            onClick={() => menu(false)}
             className={`lg:hidden rounded-full border border-black font-poppins text-2xl text-teal-950 font-bold`}
           >
             <span className="px-3 flex">
@@ -42,15 +41,17 @@ const Menu = ({ nav, menu }: Props) => {
           </Link>
         </div>
 
-        <div className="lg:hidden md:hidden mt-1">
-          <Link to={"/dashboard"}>
-            <p className={`bi-person-fill text-black text-xl`}></p>
-          </Link>
-        </div>
+        {username !== null && (
+          <div className="lg:hidden md:hidden mt-1">
+            <Link to={"/dashboard"}>
+              <p className={`bi-person-fill text-black text-xl`}></p>
+            </Link>
+          </div>
+        )}
       </div>
 
       {/* Menu Lists */}
-      <div className="relative pt-7 px-3 h-[600px]">
+      <div className="relative pt-7 px-3 h-[620px]">
         {nav.map(
           (n) =>
             n.id !== 1 && (
@@ -83,15 +84,15 @@ const Menu = ({ nav, menu }: Props) => {
         )}
 
         <div className="absolute bottom-10">
-          <div className="w-80 bg-stone-400 rounded px-4 py-4 mb-5">
-            <p className="chakra font-extrabold">
+          <div className="w-80 bg-stone-400 rounded px-4 py-4 mb-14">
+            <p className="chakra font-extrabold text-white">
               <span className="bi-telephone-fill me-5"></span> Contact Us
             </p>
-            <p className="text-sm mt-4">
+            <p className="text-sm mt-4 text-gray-200">
               Get in touch with our 24/7 customer service
             </p>
             <Link to={"/contact-us"}>
-              <p className="bg-sky-700 text-center mt-5 rounded p-2 shadow shadow-gray-50">
+              <p className="bg-sky-700 text-center mt-5 rounded p-2 shadow shadow-gray-50 text-white">
                 Contact Us
               </p>
             </Link>
