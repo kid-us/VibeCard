bf99aa788587c1bc1401331dc6cfa590f4f36bbe
kid-us <kidushh29@gmail.com>
2024-06-21 16:08:21 +0300

    Changing the loading animation component happening place

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import { useEffect, useState } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./components/Pages/Home";
 import Login from "./components/Pages/Login";
@@ -9,37 +8,10 @@ import Page404 from "./components/Pages/Page404";
 import CheckEmail from "./components/Pages/CheckEmail";
 import Create from "./components/Pages/Create";
 import Verify from "./components/Pages/Verify";
-import axios from "axios";
-import { baseUrl } from "./services/request";
-import { useUserData } from "./store/useUserData";
-import Loading from "./components/Loading/Loading";
 
 function App() {
-  const { updateEmail, updateUsername, updateType } = useUserData();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    axios
-      .get(`${baseUrl}/api/v1/auth/me`, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        withCredentials: true,
-      })
-      .then((response) => {
-        updateEmail(response.data.email);
-        updateUsername(response.data.username);
-        updateType(response.data.type);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setLoading(false);
-        console.log(err);
-      });
-  }, []);
   return (
     <>
-      {loading && <Loading />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
