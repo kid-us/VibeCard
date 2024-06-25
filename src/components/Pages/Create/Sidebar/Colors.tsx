5727897532491b82e78c355b70911e6a2cfafe67
kid-us <kidushh29@gmail.com>
2024-06-25 21:31:40 +0300

    Adding Company text color to the sidebar

--- src/components/Pages/Create/Sidebar/Colors.tsx
+++ src/components/Pages/Create/Sidebar/Colors.tsx
@@ -6,7 +6,14 @@ import { useCardColorStore } from "../../../../store/useCardColorStore";
 import { useCoverColorStore } from "../../../../store/useCoverColorStore";
 import { useState } from "react";
 
-const texts = ["Pronoun", "Name", "Location", "Job Title", "Tag Line"];
+const texts = [
+  "Pronoun",
+  "Name",
+  "Location",
+  "Job Title",
+  "Company",
+  "Tag Line",
+];
 
 const Colors = () => {
   const [dropdown, setDropdown] = useState(false);
