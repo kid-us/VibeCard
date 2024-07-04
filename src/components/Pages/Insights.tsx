fea0aed5c6df7d26d29fc8fbdc2defc5d39725a3
kid-us <kidushh29@gmail.com>
2024-07-04 11:31:27 +0300

    Setting

--- src/components/Pages/Insights.tsx
+++ src/components/Pages/Insights.tsx
@@ -20,10 +20,54 @@ const Insights = () => {
         console.log(err);
       });
   }, []);
+
+  console.log(cards);
+
   return (
     <>
       <Navbar bg="bg-white shadow" />
-      <div>Insights</div>
+      <div className="px-6 mt-5">
+        <div className="grid grid-cols-5 gap-x-5">
+          <div className="col-span-2"></div>
+          <div className="col-span-3">
+            <div className="grid grid-cols-3 p-5 gap-x-5">
+              {/* Card Views */}
+              <div className="w-full shadow rounded-xl px-5 py-4">
+                <p className="chakra">Card Views</p>
+                <h1 className="font-poppins text-3xl mt-3 font-extrabold">0</h1>
+              </div>
+              {/* Link taps */}
+              <div className="w-full shadow rounded-xl px-5 py-4">
+                <p className="chakra">Links taps</p>
+                <h1 className="font-poppins text-3xl mt-3 font-extrabold">0</h1>
+              </div>
+              {/* Contact Downloaded */}
+              <div className="w-full shadow rounded-xl px-5 py-4">
+                <p className="chakra">Contact Downloaded</p>
+                <h1 className="font-poppins text-3xl mt-3 font-extrabold">0</h1>
+              </div>
+
+              {/* Link Taps */}
+              <div className="col-span-3 shadow rounded mt-5 p-4">
+                <div className="grid grid-cols-4">
+                  <div>
+                    <h1 className="text-sm text-gray-500">Link</h1>
+                  </div>
+                  <div>
+                    <h1 className="text-sm text-gray-500">Name</h1>
+                  </div>
+                  <div>
+                    <h1 className="text-sm text-gray-500">Job</h1>
+                  </div>
+                  <div>
+                    <h1 className="text-sm text-gray-500">Taps</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/Pages/Setting.tsx
+++ src/components/Pages/Setting.tsx
@@ -1,5 +1,193 @@
+import { z } from "zod";
+import { FieldValues, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+// import Button from "../../Button/Button";
+// import axios from "axios";
+// import { baseUrl } from "../../../services/request";
+// import { useNavigate } from "react-router-dom";
+// import { baseUrl } from "../../services/request";
+import Button from "../Button/Button";
+import Navbar from "../Navbar/Navbar";
+
+const schema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Username must be greater than 3 characters." }),
+  email: z.string().email({ message: "Email address required." }),
+  password: z.string().min(8, {
+    message: "Password must be greater than 8 characters.",
+  }),
+});
+
+type FormData = z.infer<typeof schema>;
+
 const Setting = () => {
-  return <div>Setting</div>;
+  // const navigate = useNavigate();
+
+  // const [registerError, setRegisterError] = useState("");
+
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState(false);
+  const [loader, setLoader] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const onSubmit = (data: FieldValues) => {
+    if (data.password !== confirmPassword) {
+      return setConfirmPasswordError(true);
+    } else {
+      setConfirmPasswordError(false);
+      setLoader(true);
+      // axios
+      //   .get(`${baseUrl}/api/v1/auth/check-email/${data.email}`, {
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //   })
+      //   .then(() => {
+      //     axios
+      //       .post(`${baseUrl}/api/v1/auth/register`, data, {
+      //         headers: {
+      //           "Content-Type": "application/json",
+      //         },
+      //       })
+      //       .then(() => {
+      //         navigate(`/verify?email=${data.email}`);
+      //       })
+      //       .catch((error) => {
+      //         console.log(error);
+      //       });
+      // })
+      // .catch(() => {
+      //   setLoader(false);
+      //   setRegisterError("Email already exist.");
+      // });
+    }
+  };
+
+  return (
+    <>
+      <Navbar bg="bg-white shadow" />
+      <div className="menu-bg">
+        <div className="container mx-auto lg:p-10">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="bg-white py-10 lg:px-40 px-10 rounded shadow"
+          >
+            <h1 className="mb-10 text-2xl">Account Settings</h1>
+            {/* Email exist error */}
+            {/* {registerError !== "" && (
+              <div className="relative">
+                <p className="absolute -top-7 text-red-600 text-sm">
+                  <span className="bi-exclamation-triangle-fill me-4"></span>
+                  {registerError}
+                </p>
+              </div>
+            )} */}
+
+            {/* Username */}
+            <div className="mb-5">
+              <label className="text-sm text-gray-500 block" htmlFor="username">
+                Username
+              </label>
+              <input
+                {...register("username")}
+                type="text"
+                name="username"
+                className={`bg-gray-100 py-3 rounded-lg w-full focus:outline-none px-5 mt-1 block shadow-sm shadow-gray-300 font-poppins text-sm ${
+                  errors.email && "border-red-600 border-1 border"
+                }`}
+              />
+              {errors.username && (
+                <p className="text-red-600 text-xs pt-1">
+                  {errors.username.message}
+                </p>
+              )}
+            </div>
+
+            {/* Email */}
+            <div className="mb-5">
+              <label className="text-sm text-gray-500 block" htmlFor="email">
+                Email
+              </label>
+              <input
+                {...register("email")}
+                type="email"
+                name="email"
+                className={`bg-gray-100 py-3 rounded-lg w-full focus:outline-none px-5 mt-1 block shadow-sm shadow-gray-300 font-poppins text-sm ${
+                  errors.email && "border-red-600 border-1 border"
+                }`}
+              />
+              {errors.email && (
+                <p className="text-red-600 text-xs pt-1">
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+
+            {/* Password */}
+            <div className="mb-5 relative">
+              <label className="text-sm text-gray-500 block" htmlFor="password">
+                Password
+              </label>
+              <input
+                {...register("password")}
+                type={showPassword ? "text" : "password"}
+                name="password"
+                className={`bg-gray-100 py-3 rounded-lg w-full focus:outline-none px-5 mt-1 block shadow-sm shadow-gray-300 font-poppins text-sm ${
+                  errors.password && "border-red-600 border-1 border"
+                }`}
+              />
+              <span
+                onClick={() => setShowPassword(!showPassword)}
+                className={`absolute ${
+                  showPassword ? "bi-eye" : "bi-eye-slash"
+                } right-2 top-8 cursor-pointer`}
+              ></span>
+              {errors.password && (
+                <p className="text-red-600 text-xs pt-1">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+
+            {/* Confirm Password */}
+            <div className="mb-5">
+              <label className="text-sm text-gray-500 block" htmlFor="password">
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                name="confirm-password"
+                className={`bg-gray-100 py-3 rounded-lg w-full focus:outline-none px-5 mt-1 block shadow-sm shadow-gray-300 font-poppins text-sm ${
+                  confirmPasswordError && "border-red-600 border-1 border"
+                }`}
+                onChange={(event) =>
+                  setConfirmPassword(event.currentTarget.value)
+                }
+              />
+
+              {confirmPasswordError && (
+                <p className="text-red-600 text-xs pt-1">
+                  Password does not match!
+                </p>
+              )}
+            </div>
+
+            {/* Button */}
+            <Button loader={loader} label="Update" />
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Setting;
