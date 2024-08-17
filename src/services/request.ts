cc497c063445fc7af64348880993a8df806990fa
kid-us <kidushh29@gmail.com>
2024-08-17 16:16:05 +0300

    Base url changed

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,2 +1,2 @@
-export const baseUrl = "https://1ab3-102-218-50-216.ngrok-free.app";
-// export const baseUrl = "https://vibeapi.jamescog.com";
+// export const baseUrl = "https://1ab3-102-218-50-216.ngrok-free.app";
+export const baseUrl = "https://vibeapi.jamescog.com";
